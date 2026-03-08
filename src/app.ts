d12849e2825d14180372a36a29857262793371b1
almamun528 <ayapths@gmail.com>
2026-03-08 19:50:07 +0600

    better auth config done

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
+import { auth } from "./lib/auth";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, //frontend URL
+    credentials: true,
+  }),
+);
+// authentication url from better auth.. 
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
 
 app.get("/", (req, res) => {
